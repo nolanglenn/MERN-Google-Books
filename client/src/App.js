31dfcc57c4d33554188cf63d1b9a0a52cad5163c
nolanglenn <nolangglenn@gmail.com>
2019-05-11 17:07:54 -0500

    App layout complete w/ routes. Need to create individual components and API

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,26 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar/index";
+import Search from "./Components/Search/index";
+import Saved from "./Components/Saved/index"
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Search} />
+            <Route exact path="/saved" component={Saved} />
+            <Route exact path="/saved/:id" component={Saved} />
+            <Route component={NoMatch} />
+          </Switch>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Router>
     );
-  }
-}
+  };
+};
 
 export default App;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -11,14 +12,13 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+// Connect to Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooksearch");
+
 // Define API routes here
 
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
 
+// Start API server
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
